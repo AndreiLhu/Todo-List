68eefa8b62cac83626dcedfcb4c5b7559a968921
AndreiLhu <leahu.andreilucian@gmail.com>
2023-01-10 15:51:09 +0100

    finished the todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { v1 as uuidv1 } from 'uuid';
 
 function App() {
+  const id = uuidv1();
+  const [todo, setTodo] = useState({
+    id: '',
+    description: '',
+    is_done: false,
+    // add deadline field
+  });
+
+  const [todos, setTodos] = useState([]);
+
+  const handleTodoOnChange = (e) => {
+    setTodo((prevState) => ({
+      ...prevState,
+      id: id,
+      description: e.target.value,
+    }));
+  };
+
+  const handleTodoSubmit = () => {
+    setTodos([...todos, todo]);
+    setTodo((prevState) => ({ ...prevState, description: '' }));
+  };
+
+  let tableHeaders = Object.keys(todo);
+  tableHeaders.push('remove');
+
+  const removeTodo = (e) => {
+    setTodos([...todos].filter((s) => s.id !== e.id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <h1>Todo List</h1>
+        <input value={todo.description} onChange={handleTodoOnChange} />
+        <button onClick={handleTodoSubmit}>Add Todo</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            {tableHeaders.map((e, i) => (
+              <th key={i}> {e} </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((e, i) => (
+            <tr>
+              <td> {e.id.split('-')[0]} </td>
+              <td> {e.description} </td>
+              <td> {e.is_done ? 'completed' : 'not complete'} </td>
+              <td onClick={() => removeTodo(e)}> x </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
